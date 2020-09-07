b50b447f8d082dc3265a2a8b3b8f83f03a8f98b4
Lamya <Lamya04@gmail.com>
2020-09-07 10:45:46 +0200

    lamya

---
+++ index.js
@@ -0,0 +1,92 @@
+// 2
+
+ 
+
+
+// 3
+
+let monArray = []
+
+// 4
+
+var monObjet = ("")
+
+ 
+
+// 5
+
+let monString = ",";
+
+// 6
+
+let monInt
+
+// 7
+
+
+// 8
+
+function name(params) {
+    
+}
+
+// 9
+
+ 
+
+// 10
+
+ 
+
+// 11
+ 
+var d = new Date();
+var date = d.getFullYear()+'-'+(d.getMonth()+1)+'-'+d.getDate();
+console.log(date);
+
+// 12
+
+function name(params) {
+    
+}
+
+// 13
+
+// 14
+
+function longueur () {
+
+
+}
+
+
+// 15
+
+console.log(Math.floor(Math.random()*101));
+ 
+
+// 16
+
+
+
+// 17
+
+ 
+
+// 18
+
+let  objet = ("nom", "age") {
+    nom =
+
+    alert("bonjour" ${`nom`})
+
+    
+
+
+}
+
+// 19
+
+// 20
+ 
+
